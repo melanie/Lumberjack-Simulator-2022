d801113472b4587aef255ac90cd4cc92d71e0395
melanie <melanie@cooperbold.com>
2022-09-16 14:23:25 -0700

    Player moves... kinda

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float forwardsForce = 40f;
+    public float sidewaysForce = 40f;
+    public Rigidbody rb;
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlayer();
+    }
+
+    void MovePlayer() {
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.AddForce(0, 0, forwardsForce * Time.deltaTime, ForceMode.VelocityChange);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            rb.AddForce(0, 0, -forwardsForce * Time.deltaTime, ForceMode.VelocityChange);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+    }
+}
